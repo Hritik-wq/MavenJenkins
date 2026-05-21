936274215bb39d20380da86551e9742f95d4c2b1
Hritik-wq <hitikpandita402@gmail.com>
2026-05-21 16:06:53 +0530

    Update SampleTest.java

--- mavinjenkinsm-8/src/test/java/Demo/SampleTest.java
+++ mavinjenkinsm-8/src/test/java/Demo/SampleTest.java
@@ -3,15 +3,12 @@ package Demo;
 import org.testng.annotations.Test;
 
 public class SampleTest {
-	
 	@Test
 	public void case1(){
 		System.out.println("Sample Test1");
 	}
-	
 	@Test
 	public void case11(){
 		System.out.println("Sample Test11");
 	}
-
 }
